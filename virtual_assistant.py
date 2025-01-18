a8fd932e6064b366d98a8980c5f5f05f292da7e1
Rohananilkarabhimgol <rohankarabhimgol@gmail.com>
2025-01-18 22:25:35 +0100

    Added music playback functionality and modified the voice and speech rate for text-to-speech

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -2,10 +2,21 @@ import os
 import speech_recognition as sr
 import pyttsx3
 import webbrowser
+import pywhatkit
 
 
 # Initializing the text-to-speech engine
 engine = pyttsx3.init()
+
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 130)     # setting up new voice rate
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
 
 # Function to convert text to speech and play it aloud
 def speak(text):
@@ -18,9 +29,24 @@ def process_command(c):
     if c.lower().startswith("open"):
         webapp_name = c.replace("open","").strip()
         webbrowser.open(f"https://www.{webapp_name}.com")
+
+    # Condition to play music on YouTube
+    elif c.lower().startswith("play"):
+        song_name = c.replace("play","").strip()
+        speak(f"playing {song_name} on youtube")
+        if song_name:
+            pywhatkit.playonyt(song_name)
+        else:
+            speak("No song found")
+    # Condition to send message on whatsapp
+    elif "send whatsapp message" in c.lower(): 
+        name = c.replace("send whatsapp message to","").strip
+        pywhatkit.sendwhatmsg(name, "Hi")
+        webbrowser.open(f"https://www.whatsapp.com")
          
 
 if __name__ == "__main__":
+    speak("Activating assistant")
     while True:
         # Obtaining audio input from the microphone
         r = sr.Recognizer()
@@ -38,6 +64,7 @@ if __name__ == "__main__":
             if "assistant" in command.lower():
                 speak("How can i help you")
                 with sr.Microphone() as source:
+                    print("Listning....")
                     audio = r.listen(source)
                     command = r.recognize_google(audio)
                     process_command(command)
